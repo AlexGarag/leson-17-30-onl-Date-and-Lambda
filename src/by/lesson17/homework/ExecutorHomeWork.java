3d1a41c4b3c22b2546899d317b29ef838e2fdeb1
alex garag <alex.garag@bk.ru>
2024-08-05 14:00:05 +0300

    Задача4 домашки готова

--- src/by/lesson17/homework/ExecutorHomeWork.java
+++ src/by/lesson17/homework/ExecutorHomeWork.java
@@ -1,7 +1,9 @@
-package by.tms.onl30.lesson17.homework;
+package by.lesson17.homework;
+
 
 import java.util.Scanner;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class ExecutorHomeWork {
@@ -9,6 +11,8 @@ public class ExecutorHomeWork {
 //        Задача 1:
 //        Пользователь вводит в консоль дату своего рождения. Программа должна вернуть дату,
 //        когда пользователю исполнится 100 лет. Использовать Date/Time API
+//        System.out.println();
+//        System.out.println("Задача 1");
 //        Scanner scanner = new Scanner(System.in);
 //        System.out.print("Enter your date of birth in the format \"YYYY-MM-DD\": ");
 //        String dateInput = scanner.nextLine();
@@ -19,6 +23,8 @@ public class ExecutorHomeWork {
 //        Задача 2:
 //        Используя Predicate среди массива чисел вывести только те, которые являются
 //                положительными
+//        System.out.println();
+//        System.out.println("Задача 2");
 //        int[] array = {1, -5, 19, -1000, 800};
 //        Predicate<Integer> isRealNumber = i -> i > 0;
 //        for (int i : array)
@@ -27,16 +33,28 @@ public class ExecutorHomeWork {
 //            }
 
 //        Задача 3:
+//        Используя Function реализовать лямбду, которая будет принимать в себя строку в
+//        формате “*сумма* BYN”(через пробел, вместо *сумма* вставить любое значение), а
+//        возвращать сумму, переведенную сразу в доллары
+//        System.out.println();
+//        System.out.println("Задача 3");
+//
+//        String regex = "[^0-9.]+";
+//        String stringSumByn = "*300* BYN";
+//        double exchangeRateBynUsd = 3.1518;
+//        Function<String, Double> exchangeByFunction = s -> Integer.parseInt(stringSumByn.replaceAll(regex, "")) / exchangeRateBynUsd;
+//        System.out.printf("%.4f", exchangeByFunction.apply(stringSumByn));
+
+//        Задача 4:
 //        Используя Consumer реализовать лямбду, которая будет принимать в себя строку в
 //        формате “*сумма* BYN”(через пробел, вместо *сумма* вставить любое значение), а
 //        выводить сумму, переведенную сразу в доллары
-        String regex = "[^0-9.]+";
-        double exchangeRateBynUsd = 3.1518;
-        String sumByn = "*300* BYN";
-        Consumer<String> exchange = s -> {
-            System.out.printf("%.4f USD", Integer.parseInt(s.replaceAll(regex, "")) / exchangeRateBynUsd);
-        };
-        exchange.accept(sumByn);
+//        System.out.println();
+//        System.out.println("Задача 4");
+//        Consumer<String> exchange = s -> {
+//            System.out.printf("%.4f USD", Integer.parseInt(s.replaceAll(regex, "")) / exchangeRateBynUsd);
+//        };
+//        exchange.accept(stringSumByn);
 //
 //        Consumer<String> exchange = (s) -> System.out.println(s.replaceFirst(finalLine, strSumUsd));
 //        exchange.accept(finalLine);
@@ -59,8 +77,4 @@ public class ExecutorHomeWork {
             return userInput;
         };
     }
-
-
 }
-
-
